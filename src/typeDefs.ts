63fbcb249b669a6c238c2bf849e91c6386468b6a
Srilekha Kirsh <skirsh@mozilla.com>
2022-08-30 10:02:55 -0700

    init aws skeleton for lambda consumer

--- src/typeDefs.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import fs from 'fs';
-import { gql } from 'apollo-server';
-
-export default gql(
-  fs.readFileSync(path.join(__dirname, '..', 'schema.graphql')).toString()
-);
